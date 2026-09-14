8aa1ff5901b344a0fa29f4f20b08f001d013314f
Omar Desoky <omardsoky05@gmail.com>
2026-09-14 10:35:35 +0300

    feat: implement heart rate monitoring task using FreeRTOS  with UART output

--- src/App/main.c
+++ src/App/main.c
@@ -9,14 +9,44 @@
 #include "../MCAL/GPIO/GPIO_int.h"
 #include "../MCAL/ADC/ADC_int.h"
 #include "../MCAL/USART/USART_int.h"
-#include "../MCAL/SYSTICK/SYSTICK_int.h"
 #include "../HAL/Heart_Rate/HEART_RATE_int.h"
+#include "../FreeRTOS/FreeRTOS.h"
+#include "../FreeRTOS/task.h"
 
-#define SAMPLE_PERIOD_MS  10U
+#define HEART_RATE_TASK_PERIOD_MS  10U
 #define USART1_CLOCK_BIT  4U
 #define SCB_CPACR (*(volatile u32*)0xE000ED88U)
 
-static volatile u8 G_u8SampleTickFlag = 0U;
+static void u32ToStr(u32 A_u32Value, char* A_pcBuffer);
+
+static void HeartRateTask(void* A_pvParameters)
+{
+    TickType_t L_xLastWakeTime = xTaskGetTickCount();
+    u16 L_u16AdcValue;
+    u16 L_u16Bpm;
+    char L_acStrBuffer[12];
+
+    (void)A_pvParameters;
+
+    for (;;)
+    {
+        if (MADC_u8Read(&L_u16AdcValue) == ADC_STATUS_OK)
+        {
+            HHeartRate_vProcessSample(L_u16AdcValue);
+
+            if (HHeartRate_u8GetBpm(&L_u16Bpm))
+            {
+                u32ToStr((u32)L_u16Bpm, L_acStrBuffer);
+                MUSART_vSendString("BPM: ");
+                MUSART_vSendString(L_acStrBuffer);
+                MUSART_vSendString("\r\n");
+            }
+        }
+
+        vTaskDelayUntil(&L_xLastWakeTime,
+                        pdMS_TO_TICKS(HEART_RATE_TASK_PERIOD_MS));
+    }
+}
 
 static void UART_PinsInit(void)
 {
@@ -70,11 +100,6 @@ static void u32ToStr(u32 A_u32Value, char* A_pcBuffer)
     A_pcBuffer[L_u8Count] = '\0';
 }
 
-static void SysTick_10ms_Callback(void)
-{
-    G_u8SampleTickFlag = 1U;
-}
-
 int main(void)
 {
     SCB_CPACR |= (0xFU << 20U);
@@ -87,36 +112,12 @@ int main(void)
 
     MADC_vInit();
 
-    MSYSTIC_Config_t L_xSysTickCfg = {
-        .InterruptEnable = INT_ENABLE,
-        .CLK_SRC = CLK_SRC_AHB_8
-    };
-    MSYSTICK_vInit(&L_xSysTickCfg);
-    MSYSTICK_vSetIntervalMulti(SAMPLE_PERIOD_MS, SysTick_10ms_Callback);
     HHeartRate_vInit();
 
-    u16 L_u16AdcValue;
-    u16 L_u16Bpm;
-    char L_acStrBuffer[12];
+    xTaskCreate(HeartRateTask, "HeartRate", 256U, NULL, 2U, NULL);
+    vTaskStartScheduler();
 
-    while (1)
+    for (;;)
     {
-        if (G_u8SampleTickFlag)
-        {
-            G_u8SampleTickFlag = 0U;
-
-            if (MADC_u8Read(&L_u16AdcValue) == ADC_STATUS_OK)
-            {
-                HHeartRate_vProcessSample(L_u16AdcValue);
-
-                if (HHeartRate_u8GetBpm(&L_u16Bpm))
-                {
-                    u32ToStr((u32)L_u16Bpm, L_acStrBuffer);
-                    MUSART_vSendString("BPM: ");
-                    MUSART_vSendString(L_acStrBuffer);
-                    MUSART_vSendString("\r\n");
-                }
-            }
-        }
     }
 }
